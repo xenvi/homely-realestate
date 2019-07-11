48bd70de6bb48c85417c50f9d71b0017ccdbd269
xenvi <49381727+xenvi@users.noreply.github.com>
2019-07-11 00:05:01 -0500

    Create footer.php
    first init

---
+++ footer.php
@@ -0,0 +1,46 @@
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="js/bootstrap-imageupload/bootstrap-imageupload.js"></script>
+
+<script src="main.js"></script>
+<script src="sell.js"></script>
+
+
+</body>
+<footer>
+<div class="footer-container">
+
+<div class="contact">Contact Us:<br><br>
+    <a href="#"><i class="fab fa-facebook-square"></i></a>
+    <a href="#"><i class="fab fa-linkedin"></i></a>
+    <a href="#"><i class="fab fa-twitter-square"></i></a>
+    <a href="#"><i class="fab fa-instagram"></i></a>
+    </div>
+
+    <div class="footer-flex">
+        <div class="left">
+            <p>About</p>
+            <p>Support</p>
+            <p>Buy</p>
+            <p>Reviews</p>
+        </div>
+        <div class="right">
+            <p>Buy</p>
+            <p>Sell</p>
+            <p>Rent</p>
+            <p>Hire Realtor</p>
+        </div>
+    </div>
+
+    <div class="bottom">
+        <hr>
+        <p><b>(999)-123-4567</b></p>
+        <p style="font-size: 0.8rem;">Copyright © 2019 Homely. All rights reserved.</p>
+    </div>
+
+</div>
+</footer>
+</html>
