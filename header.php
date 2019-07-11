0ade00a853344cf853c5ace9aa1f5fb88ddfde7e
xenvi <49381727+xenvi@users.noreply.github.com>
2019-07-11 00:04:45 -0500

    Create header.php
    first init

---
+++ header.php
@@ -0,0 +1,23 @@
+<?php 
+    include('connection.php');
+    define ("TITLE", "Homely");
+
+?>
+
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" conteent="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title> <?php echo TITLE; ?></title>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <link rel="stylesheet" type="text/css" href="login.css">
+        <link rel="stylesheet" type="text/css" href="signup.css">
+        <link rel="stylesheet" type="text/css" href="sell.css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/51eaabbc5c.js"></script>
+    </head>
